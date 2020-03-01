a3497a457b3cee124781ac5c86b5271ef735d6a0
GitDietz <thierrydna@me.com>
2020-03-01 20:17:26 +1000

    update register form
    Hidden username to be created. Error=no value for the mandatory field. Perhaps add an initial value

--- Proj_1/accounts/forms.py
+++ Proj_1/accounts/forms.py
@@ -48,6 +48,8 @@ class UserLoginForm(forms.Form):
 class UserRegisterForm(forms.ModelForm):
     email = forms.EmailField(label='Email Address', required=True) # overrides the default
     email2 = forms.EmailField(label='Confirm Email')
+    first_name = forms.CharField(label='First Name', required=True)
+    last_name = forms.CharField(label='Last Name', required=True)
     password = forms.CharField(widget=forms.PasswordInput)
     joining = forms.CharField(label='Group to join or create', max_length=100)
 
@@ -57,13 +59,16 @@ class UserRegisterForm(forms.ModelForm):
             'username',
             'email',
             'email2',
+            'first_name',
+            'last_name',
             'password',
             'joining',
         ]
+        widgets = {'username': forms.HiddenInput()}
+        initial = {'username': 'user1'}
     #possible to use a form level clean similar to the class above - validation will then show on the form itself and not the field
 
     def clean_email2(self): #this is 2 so it runs off the email2 field
-        print(self.cleaned_data)
         email = self.cleaned_data.get('email')
         email2 = self.cleaned_data.get('email2')
         print(f'first is {email}, second is {email2}')
@@ -88,3 +93,16 @@ class UserRegisterForm(forms.ModelForm):
 
         # return super(UserRegisterForm, self).clean()
         return target_group
+
+
+    def clean_username(self):
+        uname = self.cleaned_data.get('username')
+        print('in the username clean')
+        last_name = self.cleaned_data.get('first_name')
+        new_username = self.cleaned_data.get('first_name') + min(last_name[:2],'')
+        this_user = User.objects.all().filter(Q(username__iexact=new_username))
+        i=1
+        while this_user.exists():
+            this_user + str(i)
+            i+=1
+        return new_username
